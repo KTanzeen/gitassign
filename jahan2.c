6f9acde810f836df1f3faf263924f4e5af465084
KTanzeen <74912791+KTanzeen@users.noreply.github.com>
2020-11-23 19:29:07 +0600

    Create jahan2.c

---
+++ jahan2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+
+   /* variable definition: */
+   int a, b;
+   int c;
+   float f;
+ 
+   
+  
+   c = a + b;
+   printf("value of c : %d \n", c);
+
+  
+ 
+   return 0;
+}
